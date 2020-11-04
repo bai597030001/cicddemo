20ce062c1d4335c0f7cad01f5c0e7c0447cf113e
username@email.com <baijidong123@gmail.com>
2020-11-04 10:46:44 +0800

    修改代码;修改test作用域

--- src/test/java/com/example/cicddemo/CicdDemoTest.java
+++ src/test/java/com/example/cicddemo/CicdDemoTest.java
@@ -2,6 +2,8 @@ package com.example.cicddemo;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CicdDemoTest {
 
@@ -10,13 +12,17 @@ public class CicdDemoTest {
     private String filePath = System.getProperty("cicd.filePath");
     private String objectKey = System.getProperty("cicd.objectKey");
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void putTest() {
+        logger.debug(filePath);
         Assert.assertTrue(objectClient.put(filePath));
     }
 
     @Test
     public void getTest() {
+        logger.debug(objectKey);
         Assert.assertTrue(objectClient.get(objectKey));
     }
 }
